CloudCom/goose

commit bc8ed133c042e6c393d852960a58e5ae829a4187

make sure mysql time parsing is enabled for `goose status`
fixes:

    # goose status
    2015/05/14 13:10:46 sql: Scan error on column index 0: unsupported driver -> Scan pair: []uint8 -> *time.Time

cmd/goose/cmd_status.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/cloudcom/goose/lib/goose"
 	"log"
+	"net/url"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -37,6 +39,24 @@ func statusRun(cmd *Command, args ...string) {
 		log.Fatal(e)
 	}
 
+	// we depend on time parsing, so make sure it's enabled with the mysql driver
+	if conf.Driver.Name == "mysql" {
+		i := strings.Index(conf.Driver.OpenStr, "?")
+		if i == -1 {
+			i = len(conf.Driver.OpenStr)
+			conf.Driver.OpenStr = conf.Driver.OpenStr + "?"
+		}
+		i++
+
+		q, err := url.ParseQuery(conf.Driver.OpenStr[i:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		q.Set("parseTime", "true")
+
+		conf.Driver.OpenStr = conf.Driver.OpenStr[:i] + q.Encode()
+	}
+
 	db, e := goose.OpenDBFromDBConf(conf)
 	if e != nil {
 		log.Fatal("couldn't open DB:", e)
@@ -58,8 +78,8 @@ func statusRun(cmd *Command, args ...string) {
 
 func printMigrationStatus(db *sql.DB, version int64, script string) {
 	var row goose.MigrationRecord
-	q := fmt.Sprintf("SELECT tstamp, is_applied FROM goose_db_version WHERE version_id=%d ORDER BY tstamp DESC LIMIT 1", version)
-	e := db.QueryRow(q).Scan(&row.TStamp, &row.IsApplied)
+	q := "SELECT tstamp, is_applied FROM goose_db_version WHERE version_id=? ORDER BY tstamp DESC LIMIT 1"
+	e := db.QueryRow(q, version).Scan(&row.TStamp, &row.IsApplied)
 
 	if e != nil && e != sql.ErrNoRows {
 		log.Fatal(e)

commit dfb514130d887418c79099545f350938265dc8f7

revert sql compatiblity error introduced in bc8ed13

cmd/goose/cmd_status.go
@@ -79,8 +79,8 @@ func statusRun(cmd *Command, args ...string) {
 
 func printMigrationStatus(db *sql.DB, version int64, script string) {
 	var row goose.MigrationRecord
-	q := "SELECT tstamp, is_applied FROM goose_db_version WHERE version_id=? ORDER BY tstamp DESC LIMIT 1"
-	e := db.QueryRow(q, version).Scan(&row.TStamp, &row.IsApplied)
+	q := fmt.Sprintf("SELECT tstamp, is_applied FROM goose_db_version WHERE version_id=%d ORDER BY tstamp DESC LIMIT 1", version)
+	e := db.QueryRow(q).Scan(&row.TStamp, &row.IsApplied)
 
 	if e != nil && e != sql.ErrNoRows {
 		log.Fatal(e)