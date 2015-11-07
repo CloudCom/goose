bc3b084c44fc34712cb64ea94db30441b91f4be3
Patrick Hemmer <patrick.hemmer@gmail.com>
2015-11-06 23:42:16 -0500

    gofmt examples

--- _example/migrations/20130106222315_and_again.go
+++ _example/migrations/20130106222315_and_again.go
@@ -1,15 +1,14 @@
-
 package main
 
 import (
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"fmt"
 )
 
 func Up_20130106222315(txn *sql.Tx) {
-    fmt.Println("Hello from migration 20130106222315 Up!")
+	fmt.Println("Hello from migration 20130106222315 Up!")
 }
 
 func Down_20130106222315(txn *sql.Tx) {
-    fmt.Println("Hello from migration 20130106222315 Down!")
+	fmt.Println("Hello from migration 20130106222315 Down!")
 }
